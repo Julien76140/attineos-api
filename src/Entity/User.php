2d58310fa514d8f1ca4409d99fa233eee9cab52e
Julien Mallet <58588439+Julien76140@users.noreply.github.com>
2026-05-26 08:51:28 +0200

    FIX - route pattern public with parameters + add user id to response

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,6 +18,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
